73ad0561925b4dda991bb3c1ac8bc381be25e39b
Jeremy caffin <jeremy.caffin@hotmail.com>
2019-06-28 01:38:07 +0200

    modify placement token text

--- example/example.php
+++ example/example.php
@@ -19,11 +19,11 @@ $faker = \Faker\Factory::create();
 
 $template = new Template(
     1,
-    'Votre voyage avec une agence locale [quote:destination_name]',
+    'Votre voyage avec une agence locale [quote:destinationName]',
     "
-Bonjour [user:first_name],
+Bonjour [user:firstName],
 
-Merci d'avoir contacté un agent local pour votre voyage [quote:destination_name].
+Merci d'avoir contacté un agent local pour votre voyage [quote:destinationName].
 
 Bien cordialement,
 
